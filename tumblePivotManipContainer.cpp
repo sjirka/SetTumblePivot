b8454aba290f2a3ac657fbe037a19b4612b261fd
Stepan Jirka <st.jirka@email.cz>
2022-03-17 09:54:23 +0100

    WIP
    wip from work

--- tumblePivotManipContainer.cpp
+++ tumblePivotManipContainer.cpp
@@ -35,66 +35,6 @@ MStatus TumblePivotManipContainer::connectToDependNode(const MObject &node) {
 
 void TumblePivotManipContainer::draw(M3dView &view, const MDagPath &path, M3dView::DisplayStyle style, M3dView::DisplayStatus stat) {
 	MPxManipContainer::draw(view, path, style, stat);
-
-	MStatus status;
-
-	int tumbleMode;
-	status = MGlobal::executeCommand("tumbleCtx -q -localTumble tumbleContext;", tumbleMode);
-	CHECK_MSTATUS(status);
-	m_tumbleMode = static_cast<TumbleMode>(tumbleMode);
-
-	if (!m_drag) {
-		MDagPath camPath;
-		status = view.getCamera(camPath);
-		CHECK_MSTATUS(status);
-
-		MFnCamera fnCam(camPath, &status);
-		CHECK_MSTATUS(status);
-
-		m_tumblePivot = (m_tumbleMode == TumbleMode::kTumblePoint) ? fnCam.tumblePivot(&status) : fnCam.centerOfInterestPoint(MSpace::kWorld, &status);
-		CHECK_MSTATUS(status);
-	}
-
-	m_scaleFactor = m_scaleMultiplier*SCamera::scaleFactor(const_cast<M3dView&>(view), m_tumblePivot);
-
-	short x, y;
-	view.worldToView(m_tumblePivot, x, y);
-	view.viewToWorld(x + 5, y + 5, m_textPoint, MVector());
-
-	view.beginGL();
-	glPushAttrib(GL_ALL_ATTRIB_BITS);
-
-	MString text = (m_tumbleMode == TumbleMode::kTumblePoint) ? "T" : "C";
-	MColor color = (m_tumbleMode == TumbleMode::kTumblePoint) ? MColor(tumbleColor) : MColor(centerColor);
-
-	glColor3f(color.r, color.g, color.b);
-	glDisable(GL_LINE_STIPPLE);
-
-	unsigned int divisions = 20;
-
-	glBegin(GL_LINE_STRIP);
-	for (unsigned i = 0; i <= divisions; i++) {
-		double angle = 2 * M_PI / divisions*i;
-		glVertex3d(m_tumblePivot.x+sin(angle)*m_scaleFactor, m_tumblePivot.y+cos(angle)*m_scaleFactor, m_tumblePivot.z);
-	}
-	glEnd();
-	glBegin(GL_LINE_STRIP);
-	for (unsigned i = 0; i <= divisions; i++) {
-		double angle = 2 * M_PI / divisions*i;
-		glVertex3d(m_tumblePivot.x + sin(angle)*m_scaleFactor, m_tumblePivot.y, m_tumblePivot.z + cos(angle)*m_scaleFactor);
-	}
-	glEnd();
-	glBegin(GL_LINE_STRIP);
-	for (unsigned i = 0; i <= divisions; i++) {
-		double angle = 2 * M_PI / divisions*i;
-		glVertex3d(m_tumblePivot.x, m_tumblePivot.y + sin(angle)*m_scaleFactor, m_tumblePivot.z + cos(angle)*m_scaleFactor);
-	}
-	glEnd();
-
-	view.drawText(text, m_textPoint);
-
-	glPopAttrib();
-	view.endGL();
 }
 
 void TumblePivotManipContainer::preDrawUI(const M3dView &view){
